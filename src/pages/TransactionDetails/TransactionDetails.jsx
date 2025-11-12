b05daeed9532edb3f9aacde007c8f8adb85d8c24
mahadi40011 <mahadi40011@gmail.com>
2025-11-12 16:23:11 +0600

    Transaction details page dark mode imlement

--- src/pages/TransactionDetails/TransactionDetails.jsx
+++ src/pages/TransactionDetails/TransactionDetails.jsx
@@ -14,16 +14,16 @@ const TransactionDetails = () => {
   if (!transaction) return <LoadingSpinner />;
 
   return (
-    <div className="max-w-3xl mx-auto mt-12 p-8 bg-linear-to-br from-white to-gray-50 shadow-xl rounded-3xl border border-gray-200">
-      <h2 className="text-3xl font-extrabold text-gray-800 text-center mb-8">
+    <div className="max-w-3xl mx-auto mt-12 p-8 shadow-xl rounded-3xl border border-gray-200">
+      <h2 className="text-3xl font-extrabold text-center mb-8">
         Transaction Details
       </h2>
 
       <div className="space-y-6">
-        <div className="flex flex-col space-y-6 bg-gray-100 p-4 w-full rounded-xl border border-gray-100">
+        <div className="flex flex-col space-y-6 p-4 w-full rounded-xl border border-gray-100">
           {/* Type */}
           <div className="flex justify-between items-center">
-            <span className="text-gray-700 text-xl font-semibold flex items-center gap-2">
+            <span className="text-xl font-semibold flex items-center gap-2">
               {transaction.type === "Income" ? (
                 <ArrowUpCircle className="text-green-500" size={22} />
               ) : (
@@ -32,7 +32,7 @@ const TransactionDetails = () => {
               Type
             </span>
             <span
-              className={`mt-1 px-4 py-1.5 rounded-full font-semibold text-white shadow-sm ${
+              className={`mt-1 px-4 py-1.5 rounded-full font-semibold  shadow-sm ${
                 transaction.type === "Income" ? "bg-green-500" : "bg-red-500"
               }`}
             >
@@ -42,22 +42,22 @@ const TransactionDetails = () => {
 
           {/* Date */}
           <div className="flex justify-between items-center">
-            <span className="text-gray-500 font-medium flex items-center gap-2">
+            <span className=" font-medium flex items-center gap-2">
               <Calendar size={18} className="text-blue-500" />
               Date
             </span>
-            <span className="text-gray-800 font-semibold">
+            <span className=" font-semibold">
               {transaction.date}
             </span>
           </div>
 
           {/* Category */}
           <div className="flex justify-between items-center">
-            <span className="text-gray-500 font-medium flex items-center gap-2">
+            <span className=" font-medium flex items-center gap-2">
               <Tag size={18} className="text-yellow-500" />
               Category
             </span>
-            <span className="text-gray-800 font-semibold">
+            <span className="font-semibold">
               {transaction.category}
             </span>
           </div>
@@ -65,8 +65,8 @@ const TransactionDetails = () => {
 
         <div className="flex flex-col sm:flex-row gap-3">
           {/* Amount */}
-          <div className="flex flex-col bg-gray-100 p-4 w-full rounded-xl border border-gray-100">
-            <span className="text-gray-500 font-medium mb-1">Amount</span>
+          <div className="flex flex-col p-4 w-full rounded-xl border border-gray-100">
+            <span className=" font-medium mb-1">Amount</span>
             <span
               className={`text-2xl font-bold ${
                 transaction.type === "Income"
@@ -79,20 +79,20 @@ const TransactionDetails = () => {
           </div>
 
           {/* Total of category */}
-          <div className="flex flex-col items-start p-4 w-full bg-linear-to-r from-blue-50 to-indigo-50 rounded-xl border border-blue-100">
-            <span className="text-gray-500 font-medium">
+          <div className="flex flex-col items-start p-4 w-full rounded-xl border border-blue-100">
+            <span className="font-medium">
               Total Amount of this category
             </span>
-            <span className="mt-1 text-gray-900 font-bold text-lg">
+            <span className="mt-1 font-bold text-lg">
               $12,500 demo
             </span>
           </div>
         </div>
 
         {/* Description */}
-        <div className="bg-white border border-gray-100 p-4 rounded-xl shadow-sm">
-          <span className="text-gray-500 font-medium">Description</span>
-          <p className="mt-1 text-gray-700 leading-relaxed">
+        <div className="border border-gray-100 p-4 rounded-xl shadow-sm">
+          <span className="font-medium">Description</span>
+          <p className="mt-1 leading-relaxed">
             {transaction.description || "No description provided."}
           </p>
         </div>
