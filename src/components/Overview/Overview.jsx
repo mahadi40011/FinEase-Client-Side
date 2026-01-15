d6ee923256246b5ae8090e5c41160fe37714a34d
mahadi40011 <mahadi40011@gmail.com>
2026-01-15 22:56:06 +0600

    Overview section update

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { FaWallet, FaArrowDown, FaArrowUp } from "react-icons/fa";
 import useAxios from "../../hooks/useAxios";
 import useAuth from "../../hooks/useAuth";
-import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 const Overview = () => {
   const axiosInstance = useAxios();
@@ -11,69 +10,78 @@ const Overview = () => {
   const [loadingDB, setLoadingDB] = useState(true);
 
   useEffect(() => {
-    axiosInstance.get(`/my-overview?email=${user?.email}`).then((res) => {
-      const data = res.data;
+    if (!user?.email) return;
 
-      const calculated = data.reduce(
-        (acc, item) => {
-          if (item.type.toLowerCase() === "income") {
-            acc.income += item.amount;
-          } else if (item.type.toLowerCase() === "expense") {
-            acc.expense += item.amount;
-          }
-          acc.balance = acc.income - acc.expense;
-          return acc;
-        },
-        { balance: 0, income: 0, expense: 0 }
-      );
+    axiosInstance
+      .get(`/my-overview?email=${user.email}`)
+      .then(({ data }) => {
+        const calculated = data.reduce(
+          (acc, { type, amount }) => {
+            const key = type.toLowerCase();
+            if (acc[key] !== undefined) acc[key] += amount;
+            acc.balance = acc.income - acc.expense;
+            return acc;
+          },
+          { balance: 0, income: 0, expense: 0 }
+        );
 
-      setOverview(calculated);
-      setLoadingDB(false);
-    });
-  }, [axiosInstance, user]);
-
-  if (loading) return <LoadingSpinner />;
+        setOverview(calculated);
+        setLoadingDB(false);
+      })
+      .catch((err) => console.error("Error fetching overview:", err));
+  }, [axiosInstance, user?.email]); 
 
   return (
-    <div>
-      <section>
-        <h2 className="text-2xl font-semibold text-center mb-8">
+    <section className="font-inter text-natural">
+      <div className="text-center font-poppins mb-8">
+        <h2 className="text-2xl  font-semibold">
           Financial Overview
         </h2>
-        {loadingDB ? (
-          <LoadingSpinner />
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
-            {/* Total Balance */}
-            <div className="border border-gray-100 shadow-md rounded-2xl p-8 hover:shadow-lg transition">
-              <FaWallet className="text-3xl text-blue-600 mx-auto mb-3" />
-              <h3 className="text-lg font-medium ">Total Balance</h3>
-              <p className="text-2xl font-bold  mt-1">
-                ৳ {overview?.balance.toLocaleString()}
-              </p>
-            </div>
+        <p className="text-gray-500 dark:text-gray-400 mt-2 text-sm md:text-base">
+          "Keep track of your money so your money can take care of you."
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
+        {/* Total Balance */}
+        <div className="bg-app-200 shadow-md rounded-2xl p-8 hover:shadow-lg transition">
+          <FaWallet className="text-3xl text-blue-600 mx-auto mb-3" />
+          <h3 className="text-lg font-medium ">Total Balance</h3>
+          {loading && loadingDB ? (
+            <span className="loading loading-dots loading-xl"></span>
+          ) : (
+            <p className="text-2xl font-bold  mt-1">
+              ৳ {overview?.balance?.toLocaleString()}
+            </p>
+          )}
+        </div>
 
-            {/* Income */}
-            <div className="border border-gray-100 shadow-md rounded-2xl p-8 hover:shadow-lg transition">
-              <FaArrowUp className="text-3xl text-green-600 mx-auto mb-3" />
-              <h3 className="text-lg font-medium ">Total Income</h3>
-              <p className="text-2xl font-bold  mt-1">
-                ৳ {overview?.income.toLocaleString()}
-              </p>
-            </div>
+        {/* Income */}
+        <div className="bg-app-200 shadow-md rounded-2xl p-8 hover:shadow-lg transition">
+          <FaArrowUp className="text-3xl text-green-600 mx-auto mb-3" />
+          <h3 className="text-lg font-medium ">Total Income</h3>
+          {loading && loadingDB ? (
+            <span className="loading loading-dots loading-xl"></span>
+          ) : (
+            <p className="text-2xl font-bold  mt-1">
+              ৳ {overview?.income?.toLocaleString()}
+            </p>
+          )}
+        </div>
 
-            {/* Expense */}
-            <div className="border border-gray-100 shadow-md rounded-2xl p-8 hover:shadow-lg transition">
-              <FaArrowDown className="text-3xl text-red-600 mx-auto mb-3" />
-              <h3 className="text-lg font-medium ">Total Expense</h3>
-              <p className="text-2xl font-bold  mt-1">
-                ৳ {overview?.expense.toLocaleString()}
-              </p>
-            </div>
-          </div>
-        )}
-      </section>
-    </div>
+        {/* Expense */}
+        <div className="bg-app-200 shadow-md rounded-2xl p-8 hover:shadow-lg transition">
+          <FaArrowDown className="text-3xl text-red-600 mx-auto mb-3" />
+          <h3 className="text-lg font-medium ">Total Expense</h3>
+          {loading && loadingDB ? (
+            <span className="loading loading-dots loading-xl"></span>
+          ) : (
+            <p className="text-2xl font-bold  mt-1">
+              ৳ {overview?.expense?.toLocaleString()}
+            </p>
+          )}
+        </div>
+      </div>
+    </section>
   );
 };
 
